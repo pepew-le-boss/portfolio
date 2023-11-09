71019584be03071284fb8a54909696daa516f8e0
william-fargues <fargues.wlm@gmail.com>
2023-11-09 03:27:50 +0100

    header: add ability to toggle dark mode

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,5 +1,6 @@
 import "@/public/globals.css"
 import { ReactNode } from "react"
+import { ThemeProvider } from "@/components/client/common/ThemeProvider"
 import { Topography } from "@/components/server/common/Topography"
 import { i18n, Locale } from "@/root/i18n-config"
 import { getTranslation } from "@/utils/common/getTranslations"
@@ -19,14 +20,16 @@ export async function generateMetadata({ params: { lang } }: { params: { lang: L
 
 export default function RootLayout({ children, params }: { children: ReactNode; params: { lang: string } }) {
   return (
-    <html lang={params.lang}>
+    <html lang={params.lang} suppressHydrationWarning>
       <body className={`${gabarito.variable} ${sfmono.variable} font-gabarito text-darkGrey`}>
-        <main className="relative">
-          {children}
-          <div className="absolute inset-0 z-[-1] h-full opacity-5">
-            <Topography />
-          </div>
-        </main>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <main className="relative">
+            {children}
+            <div className="absolute inset-0 z-[-1] h-full opacity-5">
+              <Topography />
+            </div>
+          </main>
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/components/client/feature/header/ThemeSwitcher.tsx
+++ src/components/client/feature/header/ThemeSwitcher.tsx
@@ -1,24 +1,41 @@
 "use client"
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/client/common/DropDownMenu"
+import { useTheme } from "next-themes"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/client/common/DropDownMenu"
 import { Icon } from "@/components/server/common/Icon"
 
 export default function ThemeSwitcher() {
+  const { setTheme } = useTheme()
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="grid h-8 w-8 place-items-center rounded-full bg-darkGrey transition-all hover:opacity-80 focus:ring-2 focus:ring-darkGrey focus:ring-offset-2">
-        <Icon name="lune" className="h-auto w-5 fill-white" />
+        <Icon name="sun" className="block h-auto w-6 text-white dark:hidden" />
+        <Icon name="moon" className="hidden h-auto w-6 text-white dark:block" />
       </DropdownMenuTrigger>
-      <DropdownMenuContent onCloseAutoFocus={(event) => event.preventDefault()} onFocusOutside={(event) => event.preventDefault()}>
-        {/* {i18n.locales
-          .filter((locale) => locale !== lang)
-          .map((locale: Locale) => (
-            <DropdownMenuItem key={locale} className="grid h-8 w-8 place-items-center rounded-full bg-darkGrey transition-all data-[highlighted]:opacity-80">
-              <Link href={redirectedPathName(locale)}>
-                <Image alt={`${altImage} ${lang}`} src={`/images/${locale}.png`} width={24} height={24} className="h-auto w-6" />
-              </Link>
-            </DropdownMenuItem>
-          ))} */}
+      <DropdownMenuContent
+        className="flex flex-col gap-1"
+        onCloseAutoFocus={(event) => event.preventDefault()}
+        onFocusOutside={(event) => event.preventDefault()}
+      >
+        <DropdownMenuItem
+          className="hidden h-8 w-8 place-items-center rounded-full bg-darkGrey transition-all data-[highlighted]:opacity-80 dark:grid"
+          onClick={() => setTheme("light")}
+        >
+          <Icon name="sun" className="h-auto w-6 text-white" />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="grid h-8 w-8 place-items-center rounded-full bg-darkGrey transition-all data-[highlighted]:opacity-80 dark:hidden"
+          onClick={() => setTheme("dark")}
+        >
+          <Icon name="moon" className="h-auto w-6 text-white" />
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          className="grid h-8 w-8 place-items-center rounded-full bg-darkGrey transition-all data-[highlighted]:opacity-80"
+          onClick={() => setTheme("system")}
+        >
+          <Icon name="system" className="h-auto w-5 text-white" />
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
