5e107054830aba499bbc29b6465b02099ee3e753
william-fargues <fargues.wlm@gmail.com>
2023-11-13 04:05:02 +0100

    header: little fix on header link

--- src/components/server/feature/header/HeaderLink.tsx
+++ src/components/server/feature/header/HeaderLink.tsx
@@ -9,7 +9,7 @@ export async function HeaderLink({ number, text }: HeaderLinkProps) {
   return (
     <Link href={`#${text}`} className="group flex flex-col items-end">
       <span className="font-sfmono text-xs font-semibold leading-none transition-all ease-out group-hover:-translate-y-1">{number}</span>
-      <span className="before:text-primary font-sfmono text-base font-semibold leading-none before:absolute before:-translate-x-5 before:transition-all before:content-['//'] group-hover:before:rotate-[-180deg]">
+      <span className="before:text-primary font-sfmono text-base font-semibold leading-none before:absolute before:-translate-x-5 before:-translate-y-[1px] before:transition-all before:content-['//'] group-hover:before:rotate-[-180deg]">
         {text}
       </span>
     </Link>
