a84e461db931eaa2b308539e3a9bf69c662e1b0f
william-fargues <fargues.wlm@gmail.com>
2025-01-21 01:10:17 +0100

    add back touch event on carousel

--- src/components/client/common/Carousel.tsx
+++ src/components/client/common/Carousel.tsx
@@ -46,11 +46,11 @@ const Carousel = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEl
       {
         ...opts,
         align: "start",
-        axis: orientation === "horizontal" ? "x" : "y"
-        // watchDrag: (event) => {
-        //   // only watch drag events on touch devices (on PC can't drag carousel with mouse)
-        //   // return "TouchEvent" in window && event instanceof TouchEvent
-        // }
+        axis: orientation === "horizontal" ? "x" : "y",
+        watchDrag: (event) => {
+          // only watch drag events on touch devices (on PC can't drag carousel with mouse)
+          return "TouchEvent" in window && event instanceof TouchEvent
+        }
       },
       plugins
     )
