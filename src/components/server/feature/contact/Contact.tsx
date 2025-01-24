20cebb1156d7ac481a44333ec357579c461b11b9
william-fargues <fargues.wlm@gmail.com>
2025-01-24 02:37:26 +0100

    improve contact section

--- src/components/server/feature/contact/Contact.tsx
+++ src/components/server/feature/contact/Contact.tsx
@@ -11,25 +11,31 @@ interface ContactProps {
 
 export function Contact({ translationsContact }: ContactProps) {
   return (
-    <div className="flex flex-col items-center gap-20">
+    <div className="flex flex-col items-center gap-52">
       <div className="w-screen border-y border-foreground bg-background py-2">
         <Marquee>
           <span className="text-sm uppercase leading-none text-foreground">{translationsContact.marquee_text}</span>
           <span className="text-sm leading-none text-foreground">{`//`}</span>
         </Marquee>
       </div>
-      <Section id={headerLinks.contact} number="05" sectionName="Contact" title={translationsContact.title} subtitle={translationsContact.description}>
+      <Section
+        id={headerLinks.contact}
+        number="05"
+        sectionName={translationsContact.section_title}
+        title={translationsContact.title}
+        subtitle={translationsContact.subtitle}
+      >
         <div className="flex flex-wrap justify-center gap-5 px-10">
           <LinkButton
             href="https://www.linkedin.com/in/william-fargues-74a9b515a/"
             icon={<Icon.BrokenArrow className="h-6 w-6 rotate-45 text-background transition-all ease-out group-hover:translate-x-1 group-hover:rotate-90" />}
-            text="Me contacter sur LinkedIn"
+            text={translationsContact.contact_me_linkedin}
             isExternal
           />
           <LinkButton
             href="mailto:fargues.wlm@gmail.com"
             icon={<Icon.BrokenArrow className="h-6 w-6 rotate-45 text-background transition-all ease-out group-hover:translate-x-1 group-hover:rotate-90" />}
-            text="Me contacter par email"
+            text={translationsContact.contact_me_email}
             isExternal
           />
         </div>
