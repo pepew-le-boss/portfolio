07eebbf2e82bca9e6c8102fb7e01787a5d9f38ec
william-fargues <fargues.wlm@gmail.com>
2025-02-05 01:36:32 +0100

    improve content a bit

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -49,13 +49,13 @@ export default function RootLayout({ children, params }: { children: ReactNode; 
   )
 }
 
-// x refacto about me en cards: le texte dans une card au dessus des deux stats cards chiffres et ma photo à droite de ça et au dessus de la stat card à l'écoute du besoin
-// x refaire les titres des sections avec des chips (pourquoi pas sous titre aussi) cf: Knives Producer Website et Cal.com
+// x Refacto about me en cards: le texte dans une card au dessus des deux stats cards chiffres et ma photo à droite de ça et au dessus de la stat card à l'écoute du besoin
+// x Refaire les titres des sections avec des chips (pourquoi pas sous titre aussi) cf: Knives Producer Website et Cal.com
 // x Créer une section mes technologies phares, cf: Maquette Brooks
-// x refaire le header ? cd: Knives Producer Website et Architecture Agency
-// x inspis header: https://sevalla.com/?ref=land-book.com // https://linear.app/homepage
-// x faire que le header disparaisse quand on scroll vers le bas et réapparaisse quand on scroll vers le haut: https://www.gopidge.com/
-// x en mode mobile mettre le header en bas de l'écran ?
+// x Refaire le header ? cd: Knives Producer Website et Architecture Agency
+// x Inspis header: https://sevalla.com/?ref=land-book.com // https://linear.app/homepage
+// x Faire que le header disparaisse quand on scroll vers le bas et réapparaisse quand on scroll vers le haut: https://www.gopidge.com/
+// x En mode mobile mettre le header en bas de l'écran ?
 // x Dans la section mes projets, mettre les screens dans des mockups. cf: https://pix.framer.website/
 // x Dans la section mes projets, faire un effet de spotlight sur les images. cf: https://motion-primitives.com/docs/tilt#tilt-with-spotlight
 // Faire une animation sur les images en hover, cf: https://cal.com/fr/platform?month=2024-11&date=2024-11-28
@@ -63,9 +63,10 @@ export default function RootLayout({ children, params }: { children: ReactNode; 
 // Faire un effet de gravité en easter egg, cf: https://www.designspells.com/spells/apps-fall-to-the-bottom-of-the-screen-when-a-search-yields-no-results-on
 // Voir pour utiliser la font de https://mode.com/
 // Voir pour utiliser la font de https://fonts.google.com/specimen/Geist+Mono
+// Améliorer le dark thème
 // Contacter ce gars pour lui proposer mes services: https://www.youtube.com/watch?v=SMi72xAM3H8
-// check le seo (metadata + balisage)
-// retravailler le contenu (éviter de mentionner freelance et rester généraliste (travailler vaec vous) + insister sur la spécialisation frontend)
-// faire les traductions en anglais
-// clean le public folder
-// passer en next 15
+// x Check le seo (metadata + balisage)
+// Retravailler le contenu (éviter de mentionner freelance et rester généraliste (travailler vaec vous) + insister sur la spécialisation frontend)
+// Faire les traductions en anglais
+// Clean le public folder
+// Passer en next 15
